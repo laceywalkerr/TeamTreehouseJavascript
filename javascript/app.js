e80e754688ffcc08e9a49baf50af7df200cc0c10
Lacey Walker <bluelarksong@gmail.com>
2021-02-27 19:54:34 -0600

    added console log to app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,2 +1,12 @@
+// 1.)
 // There are two files: index.html and app.js. To run the programming in app.js, you first need to load it into index.html.
 // Add the required HTML to load the external JavaScript file into the web page. Make sure to add your code inside the <body> element.
+
+// 2.)
+// Next, in the app.js file, add the code required to print "Begin program" to the browser's JavaScript console.
+// Step 2 is done so below.
+console.log("Begin program");
+
+// 3.)
+
+// 4.)
