5c1ec0f6470e8525d7af83c37842fec93ce9e445
Lacey Walker <bluelarksong@gmail.com>
2021-02-27 19:40:37 -0600

    learning to do a self destruct message

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,17 @@
 // dialogue box, writing directly to the console, and writing to the current webpage
 // lets write a small program using these three methods!
 
-alert("Hello, thanks for visiting!");
-console.log("Hello from the console!");
-document.write("<h1>Welcome to my webpage!</h1>");
+// alert("Hello, thanks for visiting!");
+// console.log("Hello from the console!");
+// document.write("<h1>Welcome to my webpage!</h1>");
+
+// 1. Display an alert dialog with the content: "Warning! This message will self-destruct in"
+
+
+// 2. Display a "3... 2... 1..." countdown using 3 alert dialog boxes
+
+
+// 3. This statement selects the <h1> element and replaces its text with "BOOM!".
+document.querySelector("h1").textContent = "🔥BOOM!🔥";
+
+// 4. Log "Message destroyed!" to the console
