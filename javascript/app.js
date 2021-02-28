89f1073121077e7d16fc3ddaf7bf825111a98296
Lacey Walker <bluelarksong@gmail.com>
2021-02-27 19:49:45 -0600

    first part of challenge in app.js

---
+++ javascript/app.js
@@ -0,0 +1,2 @@
+// There are two files: index.html and app.js. To run the programming in app.js, you first need to load it into index.html.
+// Add the required HTML to load the external JavaScript file into the web page. Make sure to add your code inside the <body> element.
--- javascript/script.js
+++
@@ -1,7 +0,0 @@
-// three ways to output messages with JS
-// dialogue box, writing directly to the console, and writing to the current webpage
-// lets write a small program using these three methods!
-
-alert("Hello, thanks for visiting!");
-console.log("Hello from the console!");
-document.write("<h1>Welcome to my webpage!</h1>");
