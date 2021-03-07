ff8225e1d5c6811f190ad9b730faf73b594bee5b
Lacey Walker <bluelarksong@gmail.com>
2021-03-06 22:03:33 -0600

    playing with let and var vs const

--- javascript/var.js
+++ javascript/var.js
@@ -1,10 +1,16 @@
-var message = "Hello!";
-console.log(message);
-message = "Hello from JS baisics";
-console.log(message);
-var score = 0;
+// var message = "Hello!";
+// console.log(message);
+// message = "Hello from JS baisics";
+// console.log(message);
+// var score = 0;
+// score += 10;
+// score += 5;
+// var bonusPts = 100;
+// var finalScore = score + bonusPts;
+// console.log(finalScore);
+
+let score = 5;
 score += 10;
-score += 5;
-var bonusPts = 100;
-var finalScore = score + bonusPts;
-console.log(finalScore);
+console.log(score);
+
+// let and const are considered "safer" to use than var
