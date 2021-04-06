2da8a5f73370a68941bebe9f839b597c81935b51
Lacey Walker <bluelarksong@gmail.com>
2021-04-06 00:21:09 -0500

    a note about strings

--- javascript/strings.js
+++ javascript/strings.js
@@ -1,2 +1,4 @@
 // This is a simple example of a string
 console.log("Hello, world!");
+
+// When writing a string, you must be consistent. It can be written with double quotes or single quotes, but it must be consisitant. Otherwise there will be a syntax error.
