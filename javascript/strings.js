96429102e87eb70152f67c9fc12beef3ddf67122
Lacey Walker <bluelarksong@gmail.com>
2021-04-06 00:24:30 -0500

    example code

--- javascript/strings.js
+++ javascript/strings.js
@@ -2,3 +2,6 @@
 console.log("Hello, world!");
 
 // When writing a string, you must be consistent. It can be written with double quotes or single quotes, but it must be consisitant. Otherwise there will be a syntax error.
+const htmlSnippet = '<h1 class="headline">My Headline</h1>';
+const message = "I'm a programmer!";
+console.log(message);
