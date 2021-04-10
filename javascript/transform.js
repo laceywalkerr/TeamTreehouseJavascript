970a7f6b81fea4a62ecd9b6a1fc861695bc92870
Lacey Walker <bluelarksong@gmail.com>
2021-04-10 01:48:17 -0500

    practical uses

--- javascript/transform.js
+++ javascript/transform.js
@@ -2,3 +2,4 @@
 const passphrase = 'Open Season';
 console.log(passphrase.length);
 // the number 11 would appear in your console, as it counts all the characters in the string
+// the use of this could be for passwords that must reach a certain amount of characters, or to make sure a required input field is not 0
