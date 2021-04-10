22201910b5ecf0cdf1740eea36549bc3017fe332
Lacey Walker <bluelarksong@gmail.com>
2021-04-10 01:44:44 -0500

    length of string trick

---
+++ javascript/transform.js
@@ -0,0 +1,4 @@
+// here's a fun trick! want to know the ammount of characters in a string? try this
+const passphrase = 'Open Season';
+console.log(passphrase.length);
+// the number 11 would appear in your console
