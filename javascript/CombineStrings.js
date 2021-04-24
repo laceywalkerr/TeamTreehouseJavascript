0208ec5a12a86380269b74fa3a382f3fa8da0c77
Lacey Walker <bluelarksong@gmail.com>
2021-04-23 21:54:56 -0500

    adding strings together

---
+++ javascript/CombineStrings.js
@@ -0,0 +1,7 @@
+// String Concatenation: The process of combining two or more strings to create a bigger string.
+// One way to do this, is +
+'My Favorite' + 'Movies' == 'My Favorite Movies'
+
+// Lets declare two variables
+const name = prompt("What is your name?");
+const message = "Hello!";
