7525106fb9bf88406c3b1294c0180918c5f58318
Lacey Walker <bluelarksong@gmail.com>
2021-04-23 21:46:54 -0500

    storing a variable in the prompt

--- javascript/CaptureInput.js
+++ javascript/CaptureInput.js
@@ -1,2 +1,6 @@
 // we use prompt to ask the user a question, or prompt a user to input some text. Much like an alert method, only there is a text field.
 prompt("What is your name?");
+
+// prompt returns a value, so whatever the user's input. Here we are storing it in a variable, and returning the input in the form of an alert.
+const name = prompt("What is your name?");
+alert(name);
