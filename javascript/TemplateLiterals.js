44d3a13f0d00d92968807981173ad19a1a1e213e
Lacey Walker <bluelarksong@gmail.com>
2021-05-08 00:44:52 -0500

    playing with math

--- javascript/TemplateLiterals.js
+++ javascript/TemplateLiterals.js
@@ -2,8 +2,9 @@
 
 const name = prompt('What is your name?');
 
-const message = `Hello, ${name}`;
+const message = `Hello, ${name}. Quick Maths! 2+2 is 4 minus 1 that's: ${2 + 2 - 1}`;
 // ${} marks the dynamic values of a template literal
 // This process is known as interpolation
+// Interpolation: the insertion of something of a different nature into something else.
 
 console.log(message);
