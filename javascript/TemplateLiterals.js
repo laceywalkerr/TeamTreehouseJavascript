a8ac87e46f7c68d20e0f2f40e6e7677805805533
Lacey Walker <bluelarksong@gmail.com>
2021-05-08 00:41:14 -0500

    interpolation with a template literal

--- javascript/TemplateLiterals.js
+++ javascript/TemplateLiterals.js
@@ -1,1 +1,9 @@
-//Template literals are defined with backticks, instead of quotes. ` vs "
+// Template literals are defined with backticks, instead of quotes. ` vs "
+
+const name = prompt('What is your name?');
+
+const message = `Hello, ${name}`;
+// ${} marks the dynamic values of a template literal
+// This process is known as interpolation
+
+console.log(message);
