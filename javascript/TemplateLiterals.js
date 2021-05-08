2d6b6b40597b3cfe190b9d1490381410d24b8142
Lacey Walker <bluelarksong@gmail.com>
2021-05-08 00:53:20 -0500

    you can skip lines and the code is fine with it

--- javascript/TemplateLiterals.js
+++ javascript/TemplateLiterals.js
@@ -8,3 +8,7 @@ const message = `Hello, ${name}. Quick Maths! 2+2 is 4 minus 1 that's: ${2 + 2 -
 // Interpolation: the insertion of something of a different nature into something else.
 
 console.log(message);
+
+const message2 = `Hello, ${name}. Welcome to my music site. 
+I'm happy that you came back to visit, ${name}. 
+Please come again and listen to more music.`;
